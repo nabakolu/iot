c7da2b8f5909d9def21cab06aff0a5b90ffea990
nabakolu <lnabakowski@mailo.com>
2021-07-05 15:21:54 +0200

    Added execute_solution.py, which contains method execute_solution to parse a solution string from planner and send appropriate mqqt messages

---
+++ code/planner/execute_solution.py
@@ -0,0 +1,30 @@
+import re
+import paho.mqtt.client as mqtt
+
+def execute_solution (solution, client):
+    """ parse output of planner and send respective mqtt messages """
+    solution = solution.split(";;;; Solution Found")[1] # first get only the last part, in which the solution is listed
+    solution = solution.split("\n") # split each line
+    for i in range(4, len(solution)): # start at line 4, since there are 4 more lines with useless information, until the actual solutions starts
+        command, location = re.findall(r"\((.*?)\)", solution[i])[0].split(' ', 1) # split content inside bracktes into command and location
+        location = location.split('_', 1)[1] # transform lacation into real location e.g. window_east -> east
+        print(command + " :: " + location)
+
+        # depending on command send appropriate mqtt message
+        if command == "openwindow":
+            client.publish("actuators/windows/" + location + "/command", "open")
+
+        if command == "closewindow":
+            client.publish("actuators/windows/" + location + "/command", "close")
+
+        if command == "openedblinds":
+            client.publish("actuators/blinds/" + location + "/command", "open")
+
+        if command == "closeblinds":
+            client.publish("actuators/blinds/" + location + "/command", "close")
+
+        if command == "turn_heater_on":
+            client.publish("actuators/heating/" + location + "/command", "on")
+
+        if command == "turn_heater_off":
+            client.publish("actuators/heating/" + location + "/command", "off")
